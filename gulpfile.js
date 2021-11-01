e5291c12bf016382d287453e3ae072b0d2363f9e
TaielNxz <TaielNxz@hotmail.com>
2021-10-31 21:04:46 -0300

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ const cssnano = require('cssnano');
 // HTML
 const htmlmin = require('gulp-htmlmin');
 
+// JavaScript
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+
 // Images
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
@@ -16,12 +22,12 @@ const avif = require('gulp-avif');
 
 
 function css( done ) {
-    src('css/*.css')
+    src('src/css/*.css')
         .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( dest( 'build/css' ) );
     
     done()
-};
+}
 
 function html() {
     const options = { 
@@ -33,8 +39,18 @@ function html() {
         .pipe( dest('build') );
 }
 
+function js() { 
+    return gulp.src('src/js/*.js')
+        .pipe( concat('app.js') )  
+        .pipe( babel({
+            presets: ['@babel/env']
+        }) )
+        .pipe( terser() )  
+        .pipe( gulp.dest('./build/js') )    
+}
+
 function images() {
-    return src('img/*')
+    return src('src/img/*')
         .pipe( imagemin() )
         .pipe( dest('build/img') );
 }
@@ -43,7 +59,7 @@ function imagesWebp() {
     const options = {
         quality: 50
     }
-    return src('img/*.{png,jpg}')
+    return src('src/img/*.{png,jpg}')
         .pipe( webp( options ) )
         .pipe( dest('build/img') )
 }
@@ -52,20 +68,21 @@ function imagesAvif() {
     const options = {
         quality: 50
     }
-    return src('img/*.{png,jpg}')
+    return src('src/img/*.{png,jpg}')
         .pipe( avif( options ) )
         .pipe( dest('build/img'))
 }
 
 function dev() {
-    watch( 'css/*.css', css );
+    watch( 'src/css/*.css', css );
     watch( '*.html', html );
-    watch( 'img/*', images );
+    watch( 'src/img/*', images );
 }
 
 exports.css = css;
 exports.dev = dev;
 exports.html = html;
+exports.js = js;
 exports.images = images;
 exports.imagesWebp = imagesWebp;
 exports.imagesAvif = imagesAvif;
